bc577065da68ca05fec734e65f474387e89c20ea
AlbertoSMC72 <albertosmc72@gmail.com>
2025-03-04 23:31:45 -0600

    Agregar funcionalidad de gestión de géneros con rutas, controladores y servicios

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import chapterRoutes from "./chapters/routers/chapterRoutes";
 import bookRoutes from "./books/routers/bookRoutes";
 import userLikesWritersRoutes from "./suscritions/routers/user_likes_writers";
 import userLikesBooksRoutes from "./suscritions/routers/user_likes_books";
+import genersRoutes from "./geners/routers/genres";
 
 const app = express();
 app.use(express.json());
@@ -13,6 +14,7 @@ app.use("/books", bookRoutes);
 app.use("/chapters", chapterRoutes);
 app.use("/sub_writer", userLikesWritersRoutes);
 app.use("/sub_book", userLikesBooksRoutes);
+app.use("/genres", genersRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
