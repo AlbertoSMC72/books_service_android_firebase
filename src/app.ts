73ef4bfbd684c2a6e91eeb34bc8ac3b8fd882e0a
monocoto3000 <128664121+monocoto3000@users.noreply.github.com>
2025-03-06 03:03:35 -0600

    Update routes for subscriptions

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,8 @@ import express from "express";
 import userRoutes from "./users/routers/users";
 import chapterRoutes from "./chapters/routers/chapterRoutes";
 import bookRoutes from "./books/routers/bookRoutes";
-import userLikesWritersRoutes from "./suscritions/routers/user_likes_writers";
-import userLikesBooksRoutes from "./suscritions/routers/user_likes_books";
+import writerSubsRoutes from "./subscriptions/routers/writerSubsRoutes";
+import bookSubsRoutes from "./subscriptions/routers/bookSubsRoutes";
 import genersRoutes from "./geners/routers/genres";
 
 const app = express();
@@ -12,8 +12,8 @@ app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/books", bookRoutes);
 app.use("/chapters", chapterRoutes);
-app.use("/sub_writer", userLikesWritersRoutes);
-app.use("/sub_book", userLikesBooksRoutes);
+app.use("/writerSub", writerSubsRoutes);
+app.use("/bookSub", bookSubsRoutes);
 app.use("/genres", genersRoutes);
 
 const PORT = process.env.PORT || 3000;
