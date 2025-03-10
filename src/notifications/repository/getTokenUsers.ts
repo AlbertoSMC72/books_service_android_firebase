e64b70e5797e7c89cd518579bd8cef697d537abc
AlbertoSMC72 <albertosmc72@gmail.com>
2025-03-10 03:13:05 -0600

    Actualizar consulta para obtener el token de usuario por libro, cambiando la relación de la tabla de 'books' a 'users' y mejorando la legibilidad del código

--- src/notifications/repository/getTokenUsers.ts
+++ src/notifications/repository/getTokenUsers.ts
@@ -20,25 +20,25 @@ export async function getUserToken(userId: number): Promise<string | null> {
 
 export async function getWriterTokenByBook(bookId: number): Promise<string | null> {
     const connection = await pool.getConnection();
+    console.log("bookId", bookId);
     try {
-      const [rows]: any = await connection.execute(
-        `
+        const [rows]: any = await connection.execute(
+            `
         SELECT 
-          u.firebase_token
-        FROM books b
-        JOIN users u 
-          ON b.user_id = u.id
-        WHERE b.id = ?
+            u.firebase_token
+          FROM books b
+          JOIN users u 
+            ON b.author_id = u.id
+          WHERE b.id = ?
         `,
-        [bookId]
-      );
-      if (rows.length > 0 && rows[0].firebase_token) {
-        return rows[0].firebase_token;
-      } else {
-        return null;
-      }
+            [bookId]
+        );
+        if (rows.length > 0 && rows[0].firebase_token) {
+            return rows[0].firebase_token;
+        } else {
+            return null;
+        }
     } finally {
-      connection.release();
+        connection.release();
     }
-  }
-  
+}
